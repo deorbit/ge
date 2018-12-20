ad31d2e52df83f4b770c4ef9d056a58eb93853fe
Adam Hollidge <adam@anh.io>
2018-12-20 15:22:32 -0500

    Fleshed out some docstrings.

--- gecore/github_repo.py
+++ gecore/github_repo.py
@@ -12,6 +12,16 @@ from datetime import datetime
 T = TypeVar('T', bound='GitHubRepo')
 
 class GitHubRepo:
+    """GitHubRepo represents a git repository fetched from
+    a remote source or loaded into memory from the local disk.
+
+    Attributes:
+        url: source URL
+        name: local name of the repository
+        original_author: who wrote the first commit?
+        branches: list of branches
+        commits: list of all commits
+    """
     def __init__(self, url: str = None) -> None:
         self.url = url
         self.name: str = ""
@@ -20,7 +30,13 @@ class GitHubRepo:
         self.commits: List[gecore.commit.Commit] = []
 
     def clone(self: T, local_dir: str = "") -> T:
-        """Clones a git repository hosted at self.url into local_dir."""
+        """Clones a git repository hosted at self.url into local_dir.
+        Args:
+            local_dir: directory holding the .git directory
+
+        Returns:
+            A GitHubRepo object representing the repository.
+        """
         name = os.path.basename(urlparse(self.url).path)
         if local_dir == "":
             local_dir = name
@@ -38,7 +54,14 @@ class GitHubRepo:
     @staticmethod
     def from_GitPython(repo: git.Repo) -> T:
         """Converts a GitPython Repo into our GitHubRepo
-        format for in-memory use."""
+        format for in-memory use.
+
+        Args:
+            repo: a repository object produced by the GitPython package
+
+        Returns:
+            A GitHubRepo object representing the repository.
+        """
         r = GitHubRepo()
         r.branches = [r.name for r in repo.refs if r not in repo.tags]
         r.commits = [gecore.commit.Commit(
@@ -53,5 +76,12 @@ class GitHubRepo:
         return r
 
 def load_repository(repo_dir: str) -> GitHubRepo:
-    """Load a repository from disk into memory."""
+    """Load a repository from disk into memory.
+    
+    Args:
+        repo_dir: name of directory containing the .git directory
+
+    Returns:
+        A GitHubRepo representing the repository loaded from disk
+    """
     return GitHubRepo.from_GitPython(git.Repo(repo_dir))
