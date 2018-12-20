ea179324874a185d45299dbd559abfb674530383
Adam Hollidge <adam@anh.io>
2018-12-20 12:10:35 -0500

    Add commit resolver.

--- gapi/commit.py
+++ gapi/commit.py
@@ -9,7 +9,8 @@ class Commit(graphene.ObjectType):
     author = graphene.String()
     commit_hash = graphene.String()
 
-def get_commit(c: gecore.commit.Commit) -> Commit:
+def get_commit_from_core(c: gecore.commit.Commit) -> Commit:
+    """Convert a gecore Commit object to a GraphQL Commit object."""
     commit = Commit()
     commit.timestamp = c.timestamp
     commit.message = c.message
@@ -18,7 +19,22 @@ def get_commit(c: gecore.commit.Commit) -> Commit:
 
     return commit
     
+def get_commit(repo_name: str, commit_hash:str) -> Commit:
+    """Fetch a gecore Commit object using repository name and hash
+    and return it as a GraphQL Commit object."""
+    try:
+        local_dir = os.environ['GE_REPO_DIR']
+    except KeyError:
+        local_dir = "./"
+    local_path = os.path.join(local_dir, repo_name)
+    repo = gecore.github_repo.load_repository(local_path)
+    for c in repo.commits:
+        if c.commit_hash == commit_hash:
+            return get_commit_from_core(c)
+    
 def get_commits(repo_name: str) -> List[Commit]:
+    """Fetch gecore commits and return them as a list of GraphQL
+    commit objects."""
     try:
         local_dir = os.environ['GE_REPO_DIR']
     except KeyError:
@@ -28,6 +44,6 @@ def get_commits(repo_name: str) -> List[Commit]:
     commits = []
 
     for c in repo.commits:
-        commits.append(get_commit(c))
+        commits.append(get_commit_from_core(c))
         
     return commits
--- gapi/query.py
+++ gapi/query.py
@@ -1,14 +1,14 @@
 import graphene
 from gapi.repository import Repository, get_repository
-from gapi.commit import Commit
+from gapi.commit import Commit, get_commit
 
 class GiQuery(graphene.ObjectType):
     """The root query for the GraphQL service."""
     repository = graphene.Field(Repository, name=graphene.String())
-    commit = graphene.Field(Commit, name=graphene.String(), hash=graphene.String())
+    commit = graphene.Field(Commit, name=graphene.String(), commit_hash=graphene.String())
 
     def resolve_repository(self, info, name=None):
         return get_repository(name)
 
-    # def resolve_commit(self, info, name=None, hash=None):
-    #     return get
+    def resolve_commit(self, info, name=None, commit_hash=None):
+        return get_commit(name, commit_hash)
